9ff6ef317d9b97e278509e0d5fb59e0671da59af
victoriahu <vhu@twilio.com>
2022-08-28 15:11:59 -0700

    add git submodule

--- packages/simple-express-app/server.ts
+++ packages/simple-express-app/server.ts
@@ -1,12 +1,18 @@
+import { QueryPayload } from 'simple-shared-data';
 import express from 'express';
+import _ from 'lodash';
 const app = express();
 const cors = require('cors')
 const port = 3001;
 
 app.use(cors())
 
-app.get("/", (req, res) => {
-  res.json({ foo: "bar" });
+app.get("/", (_req, res) => {
+    const responseData: QueryPayload = {
+      payload: _.snakeCase("Server data returned successfully"),
+    };
+  
+    res.json(responseData);
 });
 
 app.listen(port, () => {
